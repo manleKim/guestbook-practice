bc678b04479dbbbb6ac2ea7f38ff9dc0c7d92952
manleKim <kk410910@naver.com>
2025-05-15 17:34:33 +0900

    Init: 프로젝트 초기화

---
+++ src/main/java/com/manlekim/guestbookpractice/GuestbookPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.manlekim.guestbookpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestbookPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manlekim/guestbookpractice/GuestbookPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manlekim.guestbookpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
